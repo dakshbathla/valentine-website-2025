edc2b1421320d8ac1b88a2b13f7017f9e25e6105
dakshbathla <168924849+dakshbathla@users.noreply.github.com>
2026-01-29 10:51:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dgypu63yb/video/upload/v1769663384/Steve_Lacy_-_Dark_Red__mp3.pm_oskrls.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dgypu63yb/video/upload/v1769664049/Skrillex_and_Diplo_-_Where_Are_Now_feat._Justin_Bieber__mp3.pm_uulrg5.mp3", // Music streaming URL
         startText: " Play Music",        // Button text to start music
         stopText: " Stop Music",         // Button text to stop music
         volume: 0.8                        // Volume level (0.0 to 1.0)
